0eb86789af12a6577631a233ea6e20d5310a55a5
Manuka Koushik Yadav <46778240+sonicsplasher@users.noreply.github.com>
2023-08-01 00:09:47 +0530

    Second commit

---
+++ .local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace ExcelAutomation
+{
+    public class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
